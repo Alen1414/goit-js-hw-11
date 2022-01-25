93254161f3399eaab2a024c0d0ef1692ce371d19
Alona Puhach <alen1414@gmail.com>
2022-01-24 22:21:01 -0400

    aps

--- src/api-service.js
+++ src/api-service.js
@@ -1,25 +1,33 @@
-function httpInquiry(q, page) {
-
-    const SEARCH_URL = `https://pixabay.com/api/?key=`;
-    const API_KEY = '25348834-538d9f4405bc5a9c16273efde';
-    const OPTIONS = `&q=yellow+flowers&image_type=photo&page=5&per_page=40`;
-
-    return fetch(`${SEARCH_URL}${API_KEY}${OPTIONS}`)
-        .then(r => r.json())
-       .then(console.log);
-    
-}
-httpInquiry()
-
-// fetch(`https://pixabay.com/api/?key=25348834-538d9f4405bc5a9c16273efde&q=yellow+flowers&image_type=photo`)
-// .then(r => r.json())
-// .then(console.log);
-    //     if (!response.ok) {
-    //   throw new Error(response.status);
-//     }
-//             // return response.json();
-//         })
 
         
-export default { httpInquiry };
+export default class ApiService {
+    constructor() {
+        this.searchQuery = "";
+         this.page = 1;
+    }
+    fetchArticles() {
+        console.log(this)
+        const options = {
+            method: "get",
+            url: "https://pixabay.com/api/",
+            params: {
+                key: '25348834-538d9f4405bc5a9c16273efde',
+                q: this.searchQuery,
+                
+                image_type: "photo",
+                orientation: "horizontal",
+                safesearch: true,
+                per_page: 40,
+                page: this.page,
+            }
+        }
 
+}
+     get query(){
+         return this.searchQuery;
+    }
+    
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.html';
 import imageCard from './image_card.hbs';
 import Notiflix from 'notiflix';
-// import API from './api-service';
+import APIservice from './api-service';
 
 import axios from "axios";
 // axios.get('/users')
@@ -10,29 +10,29 @@ import axios from "axios";
 //   });
 
 
-
+const apiService = new APIservice()
+console.log(apiService )
 const refs = {
-   searchForm: document.querySelector('.search-form'),
-   
+    searchForm: document.querySelector('.search-form'),
+    imageCard: document.querySelector('.gallery'),
+  loadMore: document.querySelector('[data-action="load-more]'),
 };
-
+let searchQuery = '';
 console.log(refs.form);
 refs.searchForm.addEventListener('submit', onSearch);
+// refs.loadMore.addEventListener('click',onLoadMore );
 
 function onSearch(e) {
     e.preventDeafult();
-    const query = e.currentTarget.elements.query.value;
-    console.log(query)
+    searchQuery = e.currentTarget.elements.query.value;
+    console.log(searchQuery )
     // const s = query.searchQuery.value;
-    // console.log(s)
-
-    //  const SEARCH_URL = `https://pixabay.com/api/?key=`;
-    // const API_KEY = '25348834-538d9f4405bc5a9c16273efde';
-    // const OPTIONS = `&q=yellow+flowers&image_type=photo&page=5&per_page=40`;
-
-    return fetch(`https://pixabay.com/api/?key=25348834-538d9f4405bc5a9c16273efde&q=${query}&image_type=photo`)
-    .then(r => r.json())
-    .then(console.log)
+    // console.log()
+    apiService.fetchArticles(searchQuery)
 };
 
-onSearch()
+
+
+function onLoadMore() {
+    apiService.fetchArticles(searchQuery)
+}
