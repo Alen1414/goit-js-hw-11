e3d33edfabeec5cdf06e63d04e5d767c1da49bad
Alona Puhach <alen1414@gmail.com>
2022-01-26 11:52:23 -0400

    notiflix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.html';
 import './style.css';
-import Notiflix from 'notiflix';
+import Notiflix, { Notify } from 'notiflix';
 import ApiServise from './api-service';
 
 import markup from './image_card.hbs';
@@ -33,25 +33,36 @@ refs.loadMore.addEventListener('click', onloadMore);
 
 async function onSearch(e) {
     e.preventDefault();
-
+    disable();
+    // clearArticlesContainer();
+    // apiService.query = e.currentTarget.elements.query.value;
+    // apiService.resetPage();
     clearArticlesContainer();
     apiService.query = e.currentTarget.elements.query.value;
     // console.log(query)
     apiService.resetPage()
     const hits = await apiService.fetchArticles()
     appenArticlesMarkup(hits);
+
+
+
+apiService.fetchArticles()
+.then(object => {Notify.success(`Hooray! We found ${object.totalHits} images.`);
+    enable();
+    appenArticlesMarkup(object.its);
+})
+.catch(error => {
+    Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+});
+e.currentTarget.elements.query.value = "";
 };
-
-
 
 function onloadMore() {
     apiService.fetchArticles().then(appenArticlesMarkup)
 }
 
 function appenArticlesMarkup(hits) {
-    // const image = hits.map(element => {return markup(element);}).join('');
-    // refs.imageCard.insertAdjacentHTML("beforeend", image);
-    // console.log(image);
+    
     const code =hits.map(element => {return markup(element);}).join('');
     console.log(hits)
     refs.imageCard.insertAdjacentHTML('beforeend', code);
@@ -60,3 +71,10 @@ function appenArticlesMarkup(hits) {
 function clearArticlesContainer() {
     refs.imageCard.innerHTML='';
 }
+
+function disable() {
+    refs.loadMore.classList.add('is-hidden');
+};
+function enable() {
+    refs.loadMore.classList.remove('is-hidden');
+};
